5c4bf70d4ce327a095e7a30b74e0361f0c18d59d
Sudo-Self <tech@jessejesse.com>
2024-11-20 02:21:05 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-// vite.config.js
+
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
     outDir: 'dist',
-    assetsDir: 'assets', // Ensure models are placed correctly
+    assetsDir: 'assets',
   },
 });
 
